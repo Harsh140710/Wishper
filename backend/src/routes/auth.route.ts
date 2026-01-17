32d4ac33c22f25d0f675e695b8857bc434ffb92b
Harsh140710 <sutharharsh108@gmail.com>
2026-01-17 14:51:35 +0530

    feat: integrate Clerk authentication and add user routes

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { authCallback, getMe } from "../controllers/auth.controller";
 
 const router = Router();
 
+// route looks like "/api/auth/me"
+router.get("/me", protectRoute, getMe);
+router.post("/callback", authCallback);
 
-export default router
+export default router;
