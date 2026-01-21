ce83eda7aca8e14cf4f02b2fab9d831e7977874a
Harsh <sutharharsh108@gmail.com>
2026-01-21 10:31:37 +0530

    Removes deprecated tab navigation (#5)
    Removes the old tab navigation and introduces a basic setup with Tailwind CSS and React Query.
    
    This commit initializes the project with a basic index screen and integrates Tailwind CSS for styling and React Query for data fetching.

---
+++ mobile/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-red-600 text-4xl font-bold">Hello I am Harsh</Text>
+    </View>
+  );
+}
